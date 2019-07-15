6819d9930f77e195332441bf85a185fa37fd343a
Howard Thomas <howard@Howards-MBP.home>
2019-07-15 00:50:45 -0400

    Finished Basics of Guess Game

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,4 +1,9 @@
 # GUESSING GAME
+import random
+
+CompNum = 0
+PickNum = 0
+Turns = 3
 Play = True
 
 
@@ -14,16 +19,47 @@ def Menu():
         Play = True
 
 
-# Computer
+# Computer Logic
+def ComLogic():
+    global CompNum
+    CompNum = random.randrange(0, 20)
 
-# Users Turn
+
+# Users Logic
 def UsersTurn():
-    PickNum = int(input('Please pick a number between 1 - 20 '))
+    global PickNum
+    PickNum = int(input("Please pick a number between 1 - 20 "))
     print(PickNum)
+
+
+# Number Check
+def NumberCheck():
+    global Turns
+    global Play
+    global CompNum
+    while Turns > 0:
+        if PickNum == CompNum:
+            print("You Win")
+            Play = False
+            Turns = 0
+            break
+        elif PickNum > CompNum:
+            print("Your number was higher then the guess")
+            print("Try Again")
+            Turns = Turns - 1
+            UsersTurn()
+        elif PickNum < CompNum:
+            print("Your number was lower then the guess")
+            print("Try Again")
+            Turns = Turns - 1
+            UsersTurn()
+
 
 # Game Loop
 while Play == True:
     print("Welcome to the Number Guessing Game")
     Menu()
     UsersTurn()
+    ComLogic()
+    NumberCheck()
     Menu()
